30cc0ff4913902b89ab24f865644f9007b0d1c17
KirillTer <k.tierieshchienko@akvelon.com>
2021-02-18 10:34:50 +0200

    updated karma config

--- karma.conf.ts
+++ karma.conf.ts
@@ -42,7 +42,7 @@ module.exports = (config) => {
     config.set({
         mode: "development",
         browserNoActivityTimeout: 100000,
-        browsers: ["Chrome"],
+        browsers: ["ChromeHeadless"],
         colors: true,
         frameworks: ["jasmine"],
         reporters: [
@@ -55,7 +55,7 @@ module.exports = (config) => {
             outputFile: "TESTS-report.xml",
             useBrowserName: false
         },
-        singleRun: false,
+        singleRun: true,
         plugins: [
             "karma-coverage",
             "karma-typescript",
--- test.webpack.config.js
+++ test.webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require("webpack");
 
 module.exports = {
     devtool: 'source-map',
-    mode: 'development',
     module: {
         rules: [
             {
