f9ce0960e47a0322b66c6d6d02d417ffb869b60f
Kirill Tereschenko <v-kirillt@microsoft.com>
2021-03-09 13:34:51 +0200

    36322 fix issue with tooltip (#48)
    * updated tooltiputils
    
    * added contex menu
    
    * added contex menu
    
    * added regenerator-runtime
    
    * updated tslint
    
    * updated tslint
    
    * updating tests
    
    * middle state
    
    * updated tests and context menu
    
    * updated karma config
    
    * test commit
    
    * fixed bugs related to colors and labels
    
    * updated version number
    
    * fixed issue with segment colors
    
    Co-authored-by: KirillTer <k.tierieshchienko@akvelon.com>
    Co-authored-by: Nikita Grachev <v-grniki@microsoft.com>

--- src/Sunburst.ts
+++ src/Sunburst.ts
@@ -433,7 +433,6 @@ export class Sunburst implements IVisual {
                 .attr("startOffset", "50%")
                 .attr("xlink:href", (d, i) => "#sliceLabel_" + i)
                 .text(dataPoint => dataPoint.data.name)
-                .text((d) => d.y1)
                 .each(this.wrapPathText(Sunburst.DefaultDataLabelPadding));
         }
 
@@ -557,7 +556,30 @@ export class Sunburst implements IVisual {
         data.total += newDataPointNode.value;
         newDataPointNode.children = [];
 
-        newDataPointNode.color = parentColor;
+        if (level === 1 && originParentNode.children.length > 0) {
+            const initialColor: string = this.colorPalette.getColor(name).value;
+            for (const child of originParentNode.children) {
+                const childName: string = child.value != null ? `${child.value}` : "";
+                const initialColor: string = this.colorPalette.getColor(childName).value;
+            }
+        }
+
+        if (name && level === 2 && !originParentNode.objects) {
+            const initialColor: string = this.colorPalette.getColor(name).value;
+            const parsedColor: string = this.getColor(
+                Sunburst.LegendPropertyIdentifier,
+                initialColor,
+                originParentNode.objects,
+                name
+            );
+
+            newDataPointNode.color = this.colorHelper.getHighContrastColor(
+                "foreground",
+                parsedColor,
+            );
+        } else {
+            newDataPointNode.color = parentColor;
+        }
 
         if (originParentNode.children && originParentNode.children.length > 0) {
             for (const child of originParentNode.children) {
